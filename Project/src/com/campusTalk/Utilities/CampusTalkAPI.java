2da2b4a016b8655ce5b897ea74d6e835ed5e7684
Pallavi Madasu <pama1314@colorado.edu>
2016-04-13 21:43:03 -0600

    Update file for subscription and creation of posts and replies

--- Project/src/com/campusTalk/Utilities/CampusTalkAPI.java
+++ Project/src/com/campusTalk/Utilities/CampusTalkAPI.java
@@ -1,4 +1,5 @@
 package com.campusTalk.Utilities;
+
 import java.io.InputStream;
 import java.io.*;
 
@@ -10,9 +11,9 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.campusTalk.Controller.userController;
+import com.campusTalk.Controller.*;
 
-@Path("/Login")
+@Path("/CampusTalkAPI")
 public class CampusTalkAPI{
 	@GET
 	@Path("/{param}")
@@ -24,12 +25,12 @@ public class CampusTalkAPI{
 	}
 	
 	@POST
-	@Path("/post")
+	@Path("login")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response postMsg(InputStream input)
 	{
 		StringBuilder strBuilder = new StringBuilder();
-		userController uc = new userController();
+		UserController uc = new UserController();
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(input));
 			String line = null;
@@ -44,5 +45,85 @@ public class CampusTalkAPI{
 		int statusCode = uc.authenticate(strBuilder.toString());
 		System.out.println(statusCode);
 		return Response.status(statusCode).build();	
+	}
+	
+	@POST
+	@Path("unsubscribe")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void unsubscribeUser(InputStream input)
+	{
+		StringBuilder strBuilder = new StringBuilder();		
+		ForumController forumController = new ForumController();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(input));
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				strBuilder.append(line);
+			}
+		} catch (Exception e) {
+			System.out.println("Error Parsing: - ");
+		}
+		System.out.println("Data Received: " + strBuilder.toString());
+		forumController.unsubscribe(strBuilder.toString());	
+	}
+	
+	@POST
+	@Path("subscribe")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void subscribeUser(InputStream input)
+	{
+		StringBuilder strBuilder = new StringBuilder();		
+		ForumController forumController = new ForumController();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(input));
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				strBuilder.append(line);
+			}
+		} catch (Exception e) {
+			System.out.println("Error Parsing: - ");
+		}
+		System.out.println("Data Received: " + strBuilder.toString());
+		forumController.subscribe(strBuilder.toString());	
+	}
+	
+	@POST
+	@Path("createPost")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void createPost(InputStream input)
+	{
+		StringBuilder strBuilder = new StringBuilder();		
+		PostController postController = new PostController();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(input));
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				strBuilder.append(line);
+			}
+		} catch (Exception e) {
+			System.out.println("Error Parsing: - ");
+		}
+		System.out.println("Data Received: " + strBuilder.toString());
+		postController.createPost(strBuilder.toString());	
+	}
+	
+	@POST
+	@Path("createReply")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void createReply(InputStream input)
+	{
+		StringBuilder strBuilder = new StringBuilder();		
+		ReplyController replyController = new ReplyController();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(input));
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				strBuilder.append(line);
+			}
+		} catch (Exception e) {
+			System.out.println("Error Parsing: - ");
+		}
+		System.out.println("Data Received: " + strBuilder.toString());
+		replyController.createReply(strBuilder.toString());	
 	}
 }
